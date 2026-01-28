35e993262b496591489d8cce01054303db67dca0
Subodhkd001 <subodhdashmukhe@gmail.com>
2026-01-28 14:25:37 +0000

    using config struct and better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
+use core::panic;
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
 
-    println!("Query {} and file path is {}", query, file_path);
-    println!("In file {file_path}");
+    let config = Config::build(&args).unwrap_or_else(|err: &str| {
+        println!("Problem passing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!(
+        "Query {} and file path is {}",
+        config.query, config.filename
+    );
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("Should have been able to read the file");
     println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+struct Config {
+    query: String,
+    filename: String,
 }
 
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3{
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+        
+    }
+}
